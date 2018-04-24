6861c3770faac73cedb94ff0f1ec3e178f568d8c
Rebecca Govert <becci.govert@gmail.com>
2018-04-24 15:48:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,20 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+/* deleted these lines of initial code 4/24/2018 as learned use of if and else if conditional statements. This deleted code did pass. Bootcamp labs and intro to functions used same lab.
 
+function sayHiToGrandma(string) {
+   if (string === string.toLowerCase()) {
+    return "I can\'t hear you!"
+  }
+  if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+  if (string ==="I love you, Grandma.") {
+    return "I love you, too."
+    } 
+}
+*/
 
 function sayHiToGrandma(string) {
    if (string === string.toLowerCase()) {
